8be6d1f8277083c02c5ba775284a7373abc4943a
Juan <juan.ahumada94@gmail.com>
2020-05-11 09:36:33 -0500

    Scripts de la practica

---
+++ Assets/Scripts/ColorButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorButton : MonoBehaviour
+{
+    private Image _colorImg;
+    private Button _colorBtn;
+
+    private void Awake()
+    {
+        _colorBtn = GetComponent<Button>();
+        _colorImg = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        _colorBtn.onClick.AddListener(() => GameManager.Instance.ChangeColor(_colorImg.color));
+    }
+
+}
